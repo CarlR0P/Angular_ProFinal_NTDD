73b76d8762989aecf5bb8cfc8074abe5fd89d503
FerVanegas <mafevarey@hotmail.com>
2025-05-25 16:50:19 -0500

    Componentes y servicios partida
    Se crearon los componentes y el servicio necesario para hacer funcionar la partida. Además se borró el componente ya creado de partida

---
+++ src/app/components/partida-pregunta/partida-pregunta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partida-pregunta',
+  imports: [],
+  templateUrl: './partida-pregunta.component.html',
+  styleUrl: './partida-pregunta.component.css'
+})
+export class PartidaPreguntaComponent {
+
+}

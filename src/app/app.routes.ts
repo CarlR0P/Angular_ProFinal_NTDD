5b5474ddc0948e29d3b02dd8cf7fed86f85807e0
CarlR0P <davidrp123@hotmail.com>
2025-05-24 21:37:13 -0500

    Componente Registro/Pagina de Inicio
    Se agrega el componente de Registro para que cuando el usuario pueda registrarse cuando oprime el botón. Se modifica contenido del inicio, agregando botón de registro, modificar el logo y el contenido en general. Se borran los archivos .spec.ts, ya que no se harán pruebas unitarias.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
-    {path: 'login', component: InicioSesionComponent}
+    {path: 'login', component: InicioSesionComponent},
+    {path: 'registro', component: RegistroComponent }
 ];
